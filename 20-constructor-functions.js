39e549a72c5b70c26f69940e1d37c115af9e9025
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-21 15:16:14 +0530

    comments added

--- 20-constructor-functions.js
+++ 20-constructor-functions.js
@@ -1,4 +1,10 @@
 function Laptop(serial, year, price) {
+  // constructor function
+  // typically written in PascalCase
+  // this data structure is provided by the runtime
+  // when used with new keyword, this here becomes the empty object
+  // we then assign the properties to that empty object
+  // at the end, this empty object, now filled with properties is implicitly returned
   this.serialNumber = serial;
   this.yearOfManufacture = year;
   this.price = price;
