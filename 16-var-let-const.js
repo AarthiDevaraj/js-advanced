b514c11561b4ebba3fdd0f5f4512b44dfaab1d48
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-21 14:26:34 +0530

    done

--- 16-var-let-const.js
+++ 16-var-let-const.js
@@ -11,7 +11,7 @@ console.log(i);
 let j = 20; // no hoisting
 console.log(j);
 
-const k = 200;
+const k = 200; //  initialization is mandatory
 console.log(k);
 
 let year;
@@ -25,9 +25,10 @@ const person = {
 };
 
 console.log(person);
-person.name = 'Prasad';
+person.name = 'Prasad'; //  const is a guard against assignment, not against mutation like this
 console.log(person);
 
+//  named functions are hoisted too. not applicable for function expressions
 function foo() {
   console.log('Hello', i);
 }
