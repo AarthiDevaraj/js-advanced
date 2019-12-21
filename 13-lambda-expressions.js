c11bdb94b9c2f12cd2d5ab80f62b206482df00ac
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-21 12:21:17 +0530

    java emulation

--- 13-lambda-expressions.js
+++ 13-lambda-expressions.js
@@ -3,8 +3,29 @@ function foo(val, cb) {
   cb('data from foo');
 }
 
+function fooInJavaWay(val, cb) {
+  console.log(`foo executing: ${val}`);
+
+  //  Ensure cb is a function
+  //  using typeof
+
+  if (typeof cb === 'function') {
+    cb('data from foo');
+  } else {
+    console.log('cb is not a function');
+  }
+
+  //  using instanceof
+  //   if (cb instanceof Function) {
+  //     cb('data from foo');
+  //   } else {
+  //     console.log('cb is not a function');
+  //   }
+}
+
 function myCallback(message) {
   console.log(`callback executing: ${message}`);
 }
 
 foo(1001, myCallback);
+// fooInJavaWay(1001, 100);
