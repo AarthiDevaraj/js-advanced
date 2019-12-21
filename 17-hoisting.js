00b2ce24290256a6a9d53db095a8a5187ed8405c
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-21 14:32:28 +0530

    comments

--- 17-hoisting.js
+++ 17-hoisting.js
@@ -1,8 +1,8 @@
 function foo() {
-  console.log('foo access', val);
+  console.log('foo access', val); //  val is already hoisted and is undefined at this stage
 
   if (true) {
-    var val = 2019;
+    var val = 2019; // hoisted to the function scope!
     console.log('inner block access', val);
   }
 }
