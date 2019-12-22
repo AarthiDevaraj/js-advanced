5bf9b24d2c0a9df567cace28c0030491790646b1
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-22 11:45:28 +0530

    done

--- 35-error-handling.js
+++ 35-error-handling.js
@@ -1,10 +1,11 @@
 try {
-  console.log(x);
+  throw Error('I am throwing the error');
+  //   throw 'I am throwing a string';  //  Beware of this practice
 } catch (e) {
   if (e instanceof ReferenceError) {
-    console.log('ReferenceError Handled', e);
+    console.log('ReferenceError Handled', e.message);
   } else {
-    console.log('General Error Handled', e.name);
+    console.log('General Error Handled', e.message);
   }
 } finally {
   console.log('Runs anyway');
