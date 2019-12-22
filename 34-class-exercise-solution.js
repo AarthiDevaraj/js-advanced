5833bac8707fe67af6d842e4c230512d804f9bce
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-22 11:20:24 +0530

    done

--- 34-class-exercise-solution.js
+++ 34-class-exercise-solution.js
@@ -8,8 +8,21 @@ class Book {
 }
 
 class BookStore {
-  constructor() {
+  constructor(name) {
     this.books = [];
+    this._name = name;
+  }
+
+  get name() {
+    return this._name.toUpperCase();
+  }
+
+  set name(newName) {
+    if (newName.length >= 5) {
+      this._name = newName;
+    } else {
+      console.log('Name is not right');
+    }
   }
 
   addBook(book) {
@@ -31,7 +44,7 @@ class BookStore {
   }
 }
 
-let readersParadise = new BookStore();
+let readersParadise = new BookStore("Readers' Paradise");
 readersParadise.addBook(new Book('JavaScript', 'XYZ', 100, 250));
 readersParadise.addBook(new Book('C++', 'ABC', 700, 1000));
 readersParadise.addBook(new Book('C#', 'DEF', 250, 600));
@@ -43,3 +56,10 @@ console.log(readersParadise.findBook('C++'));
 
 readersParadise.deleteBook('Java');
 readersParadise.showBooks();
+
+console.log(readersParadise.name);
+
+readersParadise.name = 'Books forever';
+
+console.log(readersParadise.name);
+console.log(readersParadise._name);
