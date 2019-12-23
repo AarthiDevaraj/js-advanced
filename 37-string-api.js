50b57332d34df7540651e3475d759acf593ecbf7
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-23 09:29:31 +0530

    done

--- 37-string-api.js
+++ 37-string-api.js
@@ -18,3 +18,17 @@ console.log(paddedString.trim());
 let longValue = 'India;UK;USA;China;Australia;Sweden';
 console.log(longValue.split(';'));
 console.log(longValue.split(';', 1)); //  limit the split, if not provided, it is unlimited and will split till the end
+
+//  searching can be done in 2 ways: using search and indexOf
+//  indexOf
+let line = 'the name that can be named is not the eternal name';
+console.log(line.indexOf('name')); //  index of the first occurrence of the search string
+console.log(line.indexOf('name', 5)); //  starting position for search. default is 0 or beginning
+
+let lineTwo = 'the NaMe that can be Named is not the eternal name';
+// console.log(line.indexOf());
+
+//  search takes a regular expression
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
+console.log(line.search('name'));
+console.log(lineTwo.search(/name/i));
