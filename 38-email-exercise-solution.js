0da9fec35a8f43cf84b4998730e88aec27705670
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-23 10:53:15 +0530

    comments added

--- 38-email-exercise-solution.js
+++ 38-email-exercise-solution.js
@@ -6,7 +6,7 @@ class EmailNotValidError extends Error {
 
 class Email {
   constructor(email) {
-    this.email = email;
+    this.email = email; //  this.email (on lhs) refers to setter
   }
 
   set email(value) {
@@ -15,8 +15,10 @@ class Email {
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     );
     if (emailRegex.test(value)) {
-      this._email = value;
+      this._email = value; //  this is the field (private implementation, although there is no way to enforce private in ES6)
     } else {
+      //  ES6 template literals
+      //  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
       throw new EmailNotValidError(`Email (${value}) is not valid`);
     }
   }
@@ -28,6 +30,9 @@ class Email {
   get sections() {
     let parts = this._email.split('@');
     let domain = parts[1].split('.', 1)[0];
+
+    //    domain, subdomain, and TLD is a dicey subject
+    //    https://en.wikipedia.org/wiki/Top-level_domain
     let tld = parts[1].slice(domain.length);
 
     return [parts[0], domain, tld];
