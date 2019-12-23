e834b84aa5da7f8d25e17e7a9b6356f932881b87
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-23 09:15:26 +0530

    done

--- 37-string-api.js
+++ 37-string-api.js
@@ -9,3 +9,12 @@ console.log(originalString); //  strings are immutable
 //  please take a look at toLocaleLowerCase and toLocaleUpperCase
 //  these apis convert cases for the language (unicode, not available in older JS implementations)
 //  Russian has lower and upper cases: https://en.wikipedia.org/wiki/Russian_alphabet
+
+//  whitespace stripping
+let paddedString = '     hello      ';
+console.log(paddedString.trim());
+
+//  splitting of string
+let longValue = 'India;UK;USA;China;Australia;Sweden';
+console.log(longValue.split(';'));
+console.log(longValue.split(';', 1)); //  limit the split, if not provided, it is unlimited and will split till the end
