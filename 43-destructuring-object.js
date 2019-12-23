22f3d9ecf5bd5f8495ffc1f0391f9a20a9de47ed
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-23 12:19:40 +0530

    done

---
+++ 43-destructuring-object.js
@@ -0,0 +1,16 @@
+let person = {
+  id: 10,
+  first_name: 'Mella',
+  last_name: 'Yoxall',
+  email: 'myoxall9@wp.com',
+  gender: 'Female',
+  ip_address: '128.221.192.193'
+};
+
+let { id, ip_address } = person;
+
+console.log(id, ip_address);
+
+let { defaultProperty = new Date() } = person;
+
+console.log(defaultProperty);
